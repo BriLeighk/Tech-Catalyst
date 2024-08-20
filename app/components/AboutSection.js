254e1462426be388db84464ae0301e0d062f2ad7
BriLeighk <kirchgessner@wisc.edu>
2024-08-20 17:46:08 -0400

    fixed about video formatting; upped playback rate

--- app/components/AboutSection.js
+++ app/components/AboutSection.js
@@ -1,4 +1,14 @@
+import { useEffect, useRef } from 'react';
+
 export default function AboutSection() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 2; // Set the playback rate
+    }
+  }, []);
+
   return (
     <div className="bg-[#140D0C] py-10 sm:py-20 pl-4">
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10 mt-0">
@@ -19,13 +29,14 @@ export default function AboutSection() {
         <div className="mt-22 lg:mt-32 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden flex items-center justify-center">
           <div className="flex items-center justify-center w-full h-full">
             <video 
-              className="w-full max-w-[48rem] rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:max-w-[40rem] border-2 border-[#C69635] shadow-lg"
+              ref={videoRef}
+              className="w-full max-w-[19rem] rounded-xl shadow-xl sm:max-w-[40rem] border-2 border-[#C69635] shadow-lg mr-4"
               src="/about-video.mp4" 
               autoPlay 
               loop 
               muted 
               playsInline
-              style={{ transform: 'scale(1.1)' }}
+              style={{ transform: 'scale(1)' }}
             >
               Your browser does not support the video tag.
             </video>
